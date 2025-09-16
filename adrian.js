cdfcdf9f57608d511d9f8cf79220c88b67ffb0df
mhasanwira48-pixel <mhasan.wira48@sma.belajar.id>
2025-09-16 09:41:06 +0000

    mkdir pterodactyl

--- adrian.js
+++ adrian.js
@@ -1,4 +1,4 @@
-require('./config')
+yarequire('./config')
 const { BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, proto, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, areJidsSameUser, getContentType } = require('@adiwajshing/baileys')
 const fs = require('fs')
 const util = require('util')
